397f0abe82a44f4566b2917a5131b622d230fc43
songjiayi1 <1973761486@qq.com>
2019-09-10 20:51:43 +0800

    消息热搜榜单

---
+++ models/hotSearchDAO.js
@@ -0,0 +1,22 @@
+var DAO = require('./DAO')
+var hotSearchDAO = {
+    hotSearch: function(callback) {
+        DAO('select user_Id from sweet order by sweet_score', null, function(err, results) {
+        if (err) {
+            callback(err, null)
+        } else {
+            callback(null, results)
+        }
+        })
+    },
+    publish: function(callback) {
+        DAO('select obj_Id from sweet order by sweet_score', null, function(err, results) {
+        if (err) {
+            callback(err, null)
+        } else {
+            callback(null, results)
+        }
+        })
+    }
+}
+module.exports = hotSearchDAO
--- routes/list.js
+++ routes/list.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport')
-var userController = require('../controllers/userController')
+var listController = require('../controllers/listControllers')
 
 // 狂热榜
 router.get('/crazy', function (req, res, next) {
-    userController.crazy(req, res)
+    listController.crazy(req, res)
 })
 // 魅力榜
 router.get('/charm', function (req, res, next) {
-    userController.charm(req, res)
+    listController.charm(req, res)
 })
 // 比翼榜
-router.get('/sweet-chart', function (req, res, next) {
-    userController.sweet-chart(req, res)
+router.get('/sweetChart', function (req, res, next) {
+    listController.sweetChart(req, res)
 })
