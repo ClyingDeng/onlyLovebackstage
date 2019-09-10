b775017816f6d6ff621afbbe4c223f9a7608fc28
ClyingDeng <1174915260@qq.com>
2019-09-10 18:52:59 +0800

    邓颖修改app.js

--- routes/search.js
+++ routes/search.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport')
-var userController = require('../controllers/userController')
+var userController = require('../controllers/searchController')
 
 // 普通用户
-router.get('/commUser',passport.authenticate('jwt',{session:false}) , function (req, res, next) {
-    userController.commUser(req, res)
+router.get('/commUser', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+    searchController.commUser(req, res)
 });
 // 一级用户
-router.get('/seniorUser',passport.authenticate('jwt',{session:false}) , function (req, res, next) {
-    userController.seniorUser(req, res)
+router.get('/seniorUser', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+    searchController.seniorUser(req, res)
 });
 // 二级用户
-router.get('/masterUser',passport.authenticate('jwt',{session:false}) , function (req, res, next) {
-    userController.masterUser(req, res)
+router.get('/masterUser', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+    searchController.masterUser(req, res)
 });
