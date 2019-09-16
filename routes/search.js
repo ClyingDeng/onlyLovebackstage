e3844ab151a3a61ea71c2835d6e31f6bbe0d0066
ClyingDeng <1174915260@qq.com>
2019-09-16 15:30:11 +0800

    搜索模块完成，主页信息完成，解锁功能完成一半

--- routes/search.js
+++ routes/search.js
@@ -4,15 +4,15 @@ var passport = require('passport')
 var searchController = require('../controllers/searchControllers')
 
 // 普通用户
-router.get('/commUser', passport.authenticate('jwt', { session: false }), function(req, res, next) {
-    searchController.commUser(req, res)
+router.get('/member', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+    searchController.getSearch(req, res)
 });
 // 一级用户
-router.get('/seniorUser', passport.authenticate('jwt', { session: false }), function(req, res, next) {
-    searchController.seniorUser(req, res)
-});
-// 二级用户
-router.get('/masterUser', passport.authenticate('jwt', { session: false }), function(req, res, next) {
-    searchController.masterUser(req, res)
-});
+// router.get('/seniorUser', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+//     searchController.seniorUser(req, res)
+// });
+// // 二级用户
+// router.get('/masterUser', passport.authenticate('jwt', { session: false }), function(req, res, next) {
+//     searchController.masterUser(req, res)
+// });
 module.exports = router;
