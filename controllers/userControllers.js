0feaca2b19e395cca425b8f980cd28a202306d6d
ClyingDeng <1174915260@qq.com>
2019-09-18 08:55:22 +0800

    基本完成了吗

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,30 +4,6 @@ var path = require('path')
 var bcrypt = require('bcrypt')
 var jwt = require('jsonwebtoken')
 var userController = {
-    getAllUsers: function(req, res) {
-        // userDAO.getAllUsers(function(err, results) {
-        //     if (err) {
-        //         res.json({ code: 500, msg: '用户信息查询失败！' })
-        //     } else {
-        //         res.json({ code: 200, data: results, msg: '用户查询成功！' })
-        //     }
-        // })
-    },
-    getUserById: function(req, res, userId) {
-        // var userId = req.params.uId
-        // userDAO.getUserById(userId, function(err, results) {
-        // if (err) {
-        //     res.json({ code: 500, msg: '用户信息查询失败！' })
-        // } else {
-        //     if (results.length > 0) {
-        //         res.json({ code: 200, data: results, msg: '用户查询成功！' })
-        //     } else {
-        //         res.json({ code: 200, data: results, msg: '查无此人！' })
-
-        //     }
-        // }
-        // })
-    },
     changeName: function(req, res) {
 
     },
@@ -84,6 +60,152 @@ var userController = {
                 })
             });
         });
-    }
+    },
+    // 修改密码
+    updatePassword: function(req, res) {
+        var userId = req.user[0].base_info_Id
+        var userpwd = req.body.userpwd
+            // var user = { userId: req.body.base_info_Id, userpwd: req.body.userpwd }
+        bcrypt.genSalt(10, function(err, salt) {
+
+            bcrypt.hash(userpwd, salt, function(err, hash) {
+                userpwd = hash
+                console.log('哈哈哈哈' + hash)
+
+
+                userDAO.updatePassword({ userId: userId, pwd: userpwd }, function(err, results) {
+
+                    if (err) {
+                        res.json({ code: 500, msg: '用户修改密码失败！' })
+                    } else {
+                        //检查该操作对数据表是否造成影响
+                        if (results.affectedRows == 0) {
+                            res.json({ code: 500, msg: '用户修改密码失败！' })
+                        } else {
+
+                            res.json({ code: 200, data: results, msg: '用户修改密码成功！' })
+                                // res.status(200).json({ msg: '注册成功！' })
+
+
+                        }
+
+
+
+                    }
+                });
+            });
+        });
+
+
+    },
+    //上传头像
+    updateHeadPic: function(req, res) {
+        var form = new formidable.IncomingForm() //创建上传表单对象
+        form.uploadDir = path.join(__dirname, '..', '/public/upload') //设置上传文件的路径
+        form.keepExtensions = true //设置保留上传文件的扩展名
+        form.parse(req, function(err, fields, files) {
+            if (err) {
+                res.send('文件上传错误！')
+            }
+            console.log('...................')
+                //fields是上传的表单字段数组，files是上传的文件列表
+            console.log(files)
+                //保存图片路径到数据库
+                //1.获取当前用户编号
+            var userId = req.user[0].base_info_Id
+                //2.获取当前用户的图片名称
+            var headPic = path.parse(files.file.path).base
+            console.log(headPic)
+            userDAO.upload({ file: headPic, userId: userId }, function(err, results) {
+                //    console.log(user.headPic)
+                if (err) {
+                    res.json({ code: 500, msg: '上传文件失败！' })
+                } else {
+                    res.json({ code: 200, data: results, msg: '上传文件成功！' })
+                }
+            })
+        })
+
+    },
+    //送出礼物
+    from_Presents: function(req, res) {
+        var userId = req.user[0].base_info_Id
+            // var gift = req.body.gift
+        userDAO.from_Presents(userId, function(err, results) {
+            if (err) {
+                res.json({ code: 500, msg: '用户送出礼物失败！' })
+            } else {
+                //检查该操作对数据表是否造成影响
+                // if (results.affectedRows == 0) {
+                //     res.json({ code: 500, msg: '用户送出礼物失败！' })
+                // } else {
+                res.json({ code: 200, data: results, msg: '用户送出礼物成功！' })
+                    // }
+            }
+        });
+
+
+    },
+    // 修改个人信息
+    updateInfo: function(req, res) {
+        var user = { userId: req.user[0].base_info_Id, nickName: req.body.nickName, age: req.body.age }
+
+
+        userDAO.updateInfo(user, function(err, results) {
+            console.log(results)
+            if (err) {
+                console.log(err)
+                res.json({ code: 500, msg: '用户修改失败!' })
+            } else {
+                if (results.affectedRows == 0) {
+                    res.json({ code: 500, msg: '用户信息修改失败' })
+                } else {
+                    res.json({ code: 200, data: results, msg: '用户信息修改成功' })
+                }
+            }
+        })
+    },
+    //用户其他信息
+    userInfo: function(req, res) {
+        // var oId = req.params.uId 2o;
+        // console.log(req.user)
+        var userId = req.user[0].base_info_Id
+        console.log(userId)
+        userDAO.userInfo_member(userId, function(err, results) {
+
+            if (err) {
+                res.json({ code: 500, msg: '搜索查询失败！' })
+            } else {
+                if (!results) {
+                    console.log('该用户不是会员')
+                } else {
+                    res.json({ code: 200, data: results, msg: '搜索查询成功！' })
+                }
+            }
+        })
+
+    },
+    //收到礼物
+    to_Presents: function(req, res) {
+        var userId = req.user[0].base_info_Id
+        console.log(userId)
+            // var gift = req.body.gift
+        userDAO.to_Presents(userId, function(err, results) {
+            if (err) {
+                console.log(err)
+                res.json({ code: 500, msg: '用户收到礼物失败！' })
+            } else {
+                //检查该操作对数据表是否造成影响
+                // if (results.affectedRows == 0) {
+                //     res.json({ code: 500, msg: '用户收到礼物失败！' })
+                // } else {
+                res.json({ code: 200, data: results, msg: '用户收到礼物成功！' })
+                    // }
+            }
+        });
+
+
+    },
+
 }
 module.exports = userController
--- models/userDAO.js
+++ models/userDAO.js
@@ -1,25 +1,6 @@
 var DAO = require('./DAO')
     //针对用户数据表操作的模块对象
 var userDAO = {
-    getAllUsers: function(callback) {
-        // DAO('select * from stu', null, function(err, results) {
-        // if (err) {
-        //     callback(err, null)
-        // } else {
-        //     callback(null, results)
-        // }
-        // })
-    },
-    getUserById: function(userId, callback) {
-        // DAO('select * from stu where stuNo = ?', [userId], function(err, results) {
-        //     console.log(results)
-        //     if (err) {
-        //         callback(err, null)
-        //     } else {
-        //         callback(null, results)
-        //     }
-        // })
-    },
     changeName: function(userId, userName, callback) {
 
     },
@@ -46,6 +27,77 @@ var userDAO = {
                 callback(null, results)
             }
         })
+    },
+    // 修改密码
+    updatePassword: function(user, callback) {
+        console.log(user)
+        DAO('update base_info set pwd = ? WHERE base_info_id = ?', [user.pwd, user.userId], function(err, results) {
+            if (err) {
+                callback(err, null)
+            } else {
+                callback(null, results)
+            }
+        })
+    },
+    // 上传头像
+    upload: function(user, callback) {
+        console.log(user)
+        DAO('update base_info set headPic = ? where base_info_Id = ?', [user.file, user.userId], function(err, results) {
+            if (err) {
+                callback(err, null)
+            } else {
+                callback(null, results)
+            }
+        })
+    },
+    //送出礼物
+    from_Presents: function(userId, callback) {
+        // console.log(user)
+        DAO('select from_Id,nickName,gift,prop_Name,sum(giftNum) from gift,base_info,props where from_Id=base_info_Id and prop_Id=gift and from_Id=? group by from_Id,gift', [userId], function(err, results) {
+            if (err) {
+                callback(err, null)
+            } else {
+                callback(null, results)
+            }
+        })
+    },
+    //收到礼物
+    to_Presents: function(userId, callback) {
+        console.log(userId)
+        DAO('select to_Id,nickName,gift,prop_Name,sum(giftNum) from gift,base_info,props where to_Id=base_info_Id and prop_Id=gift and to_Id=? group by to_Id,gift', [userId], function(err, results) {
+            if (err) {
+                callback(err, null)
+            } else {
+                callback(null, results)
+            }
+        })
+    },
+    // 修改个人信息
+    updateInfo: function(user, callback) {
+        // console.log('啊哈哈哈哈' +results)
+
+        DAO('update base_info set nickName = ?,age = ? where base_info_Id= ?', [user.nickName, user.age, user.userId], function(err, results) {
+            if (err) {
+                callback(err, null)
+            } else {
+                callback(null, results)
+            }
+        })
+    },
+    //用户其他信息
+    userInfo_member: function(userId, callback) {
+
+        DAO('SELECT member_grade from memberinfos,base_info WHERE member_user_Id = base_info_Id and base_info_Id =?', [userId], function(err, results) {
+            console.log(results)
+            if (err) {
+                console.log('无法获取用户会员等级')
+                callback(err, null)
+            } else {
+                callback(null, results)
+            }
+        })
     }
+
+
 }
 module.exports = userDAO
